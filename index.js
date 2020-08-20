44fd03a5f03812b44967eac62b48795f19ff6fb6
Tim Qian <timqian92@qq.com>
2020-08-20 16:06:34 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const dayjs = require('dayjs');
+const getHeadlines = require('./utils/getHeadlines');
+const issue = require('./utils/issue');
+
+// run every Monday at 00:01 UTC
+const run = async (date) => {
+  const contents = await getHeadlines(date);
+  console.log(contents)
+  issue.open({
+    owner: 'headllines',
+    repo: 'hackernews-weekly',
+    title: `Hacker News Weekly Top 10 @${new Date(date).toISOString().slice(0, 10)}`,
+    body: contents
+  })
+}
+
+run(new Date());
+
+// run(dayjs().subtract(3, 'days').toDate());
+// run(dayjs().subtract(10, 'days').toDate());
+// run(dayjs().subtract(17, 'days').toDate());
+// run(dayjs().subtract(24, 'days').toDate());
+// run(dayjs().subtract(31, 'days').toDate());
+// run(dayjs().subtract(38, 'days').toDate());
+// run(dayjs().subtract(45, 'days').toDate());
+// run(dayjs().subtract(7 * 7 + 3, 'days').toDate());
+// run(dayjs().subtract(7* 8 + 3, 'days').toDate());
+// run(dayjs().subtract(7* 9 + 3, 'days').toDate());
+// run(dayjs().subtract(7* 10 + 3, 'days').toDate());
