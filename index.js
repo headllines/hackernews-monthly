b9ea8013052e4eb61efe72c12a8f9a780f2a3fbe
timqian <timqian92@qq.com>
2020-09-01 18:41:33 +0800

    remove and invalidate secrets

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const issue = require('./utils/issue');
 // run every first day of month at 00:01 UTC
 const run = async (date) => {
   const contents = await getHeadlines(date);
-  console.log(contents.length)
-  issue.open({
+  console.log(contents.length);
+  const res = await issue.open({
     owner: 'headllines',
     repo: 'hackernews-monthly',
     title: `Hacker News Monthly Top 10 @${new Date(date).toISOString().slice(0, 10)}`,
     body: contents
-  })
+  });
+  const issueNumber = res.data.number;
+
+  await issue.lock({
+    owner: 'headllines',
+    repo: 'hackernews-monthly', 
+    issueNumber,
+  });
 }
 
 run(new Date());
